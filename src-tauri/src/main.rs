1e540d74089e435b8aab0840ea3c4ef4acc06409
RenanSui <renanddtao@gmail.com>
2023-12-13 14:22:48 -0300

    add window_vibrancy create

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,7 @@ use dotenvy::dotenv;
 use std::env;
 use tauri::{App, AppHandle, LogicalPosition, Manager, SystemTray, SystemTrayEvent};
 use window_shadows::set_shadow;
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
 
 fn main() {
     let tray: SystemTray = SystemTray::new();
@@ -21,6 +22,14 @@ fn main() {
 
 fn setup_window(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     let window = app.get_window("main").unwrap();
+
+    #[cfg(target_os = "macos")]
+    apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+    #[cfg(target_os = "windows")]
+    apply_blur(&window, Some((18, 18, 18, 125)))
+        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 
     set_shadow(&window, true).expect("Unsupported platform!");
 
